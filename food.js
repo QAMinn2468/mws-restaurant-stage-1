b04436b5f22fa04593dbfe14f27164e3d50ed9a8
Kimberly McCaffrey <Kimberly.M.McCaffrey@gmail.com>
2018-09-01 18:32:58 -0400

    Mentor Session - half off line

--- food.js
+++ food.js
@@ -16,37 +16,41 @@ const cachePrimary = [
   '/img/8.jpg',
   '/img/9.jpg',
   '/img/10.jpg',
-  '/services/food.js',
-  'app.js',
-  'CODEOWNERS',
-  'index.html',
-  'README.md',
-  'restaurant.html',
-  'router.js'
+  'food.js',
+  // 'app.js',
+  // 'CODEOWNERS',
+  '/index.html',
+  // '/README.md',
+  '/restaurant.html'
+  // 'router.js'
+
 ];
 
 self.addEventListener('install', (event => {
   event.waitUntil(
-
   caches.open('version1').then(cache => {
     console.log('this is working!');
     return cache.addAll(cachePrimary);           //This is working!!
   }));
-}));
+}))
 
 self.addEventListener('fetch', function(event) {
-
-  // event.respondWith(
-  //   caches.match(event.request).then(function(response) {
-  //     if (response) {
-  //       return response;
-  //     }
-  //     else {
-  //       return fetch(event.request)
-  //   }}).catch(function(error) {
-  //     console.log(`There was an error in fetching from the cache. Error: ${err}`);
-  //   }));
+  console.log(event.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // console.log(response);
+      if (response) {
+        console.log('this is my response!');    // This logs.
+        return response;
+      }
+      else {
+        return fetch(event.request);
+    }}).catch(function(error) {
+      console.log(`There was an error in fetching from the cache. Error: ${error}`);
+    }));
   });
 
 
     // console.log('this is a test!');
+
+    // https://unpkg.com/leaflet@1.3.1/dist/leaflet.js
